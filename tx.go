8d82a394042feb858f8b8b40f199f9ede5cc1b83
Matt Joiner <anacrolix@gmail.com>
2021-09-04 21:09:25 +1000

    Make Operation generic
    From https://github.com/anacrolix/stm/commit/80e033aa1f2218b83fb5891671ed795de72e19d5

--- tx.go
+++ tx.go
@@ -114,7 +114,7 @@ type txProfileValue struct {
 
 // Retry aborts the transaction and retries it when a Var changes. You can return from this method
 // to satisfy return values, but it should never actually return anything as it panics internally.
-func (tx *Tx) Retry() interface{} {
+func (tx *Tx) Retry() struct{} {
 	retries.Add(txProfileValue{tx, tx.numRetryValues}, 1)
 	tx.numRetryValues++
 	panic(retry)
